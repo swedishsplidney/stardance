560ceae5bdd7ac06e8b94f026a41f92773bc237a
Kartikey Chauhan <me@cskartikey.dev>
2026-05-31 05:22:37 +0530

    refactor(posts): reuse composer for quote reposts

--- app/components/posts/composer_component.rb
+++ app/components/posts/composer_component.rb
@@ -4,18 +4,59 @@ module Posts
   class ComposerComponent < ViewComponent::Base
     delegate :inline_svg_tag, to: :helpers
 
-    attr_reader :post, :current_user, :projects, :selected_project, :test_time_granted
+    attr_reader :post, :current_user, :projects, :selected_project, :test_time_granted,
+                :url, :scope, :aria_label, :body_label, :placeholder, :submit_text,
+                :disable_with, :simple_mode, :show_project_chips, :show_attachments,
+                :show_time_preview
 
-    def initialize(post:, current_user:, projects:, selected_project:, test_time_granted: false)
+    def initialize(post:, current_user:, projects:, selected_project:, test_time_granted: false,
+      url: nil, scope: nil, aria_label: "Create a devlog", body_label: "What are you working on?",
+      placeholder: "What are you working on?", submit_text: "Post", disable_with: "Posting...",
+      simple_mode: false, show_project_chips: true, show_attachments: true, show_time_preview: true)
       @post = post
       @current_user = current_user
       @projects = projects
       @selected_project = selected_project
       @test_time_granted = test_time_granted
+      @url = url
+      @scope = scope
+      @aria_label = aria_label
+      @body_label = body_label
+      @placeholder = placeholder
+      @submit_text = submit_text
+      @disable_with = disable_with
+      @simple_mode = simple_mode
+      @show_project_chips = show_project_chips
+      @show_attachments = show_attachments
+      @show_time_preview = show_time_preview
     end
 
     def enabled?
-      selected_project.present? && !setup_pending?
+      if simple_mode?
+        current_user.present?
+      else
+        selected_project.present? && !setup_pending?
+      end
+    end
+
+    def simple_mode?
+      simple_mode
+    end
+
+    def show_project_chips?
+      show_project_chips && projects.any?
+    end
+
+    def show_attachments?
+      show_attachments
+    end
+
+    def show_time_preview?
+      show_time_preview
+    end
+
+    def form_url
+      url || helpers.project_devlogs_path(selected_project)
     end
 
     # Guest user who has already started the first-project setup flow but
@@ -31,7 +72,9 @@ module Posts
     end
 
     def preview_time_url
-      helpers.preview_time_project_devlogs_path(selected_project)
+      if selected_project.present?
+        helpers.preview_time_project_devlogs_path(selected_project)
+      end
     end
   end
 end
