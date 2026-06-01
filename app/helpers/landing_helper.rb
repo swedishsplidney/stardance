a9bc94c979bf0e5c560bfd500f0311480bfb5fdb
EnterpriseGoose <olive@hackclub.com>
2026-06-01 13:52:21 -0400

    fix(landing): remove fragment caching from homepage

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -9,11 +9,4 @@ module LandingHelper
     { url: "https://github.com/SteveMan67/platformed", mod: 5, by: "By Steve, 16", img: "landing/projects/steve.avif", alt: "Project by Steve", desc: "A platformer with a fully functioning level editor, online level sharing, and trigger system, built from the ground up in Javascript for Flavortown." }
   ].freeze
 
-  def landing_static_sections_cache_key
-    [
-      "landing/static-sections/v1",
-      @new_onboarding,
-      Rails.application.config.git_version
-    ]
-  end
 end
